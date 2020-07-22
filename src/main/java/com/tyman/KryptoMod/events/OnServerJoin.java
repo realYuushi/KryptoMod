8deb8427403533519b5af8772eb49802b1843bbf
Tyler Beckan ohio <tbeckman530@gmail.com>
2020-07-22 14:35:26 -0400

    Remove logging, change event to world load

--- src/main/java/com/tyman/KryptoMod/events/OnServerJoin.java
+++ src/main/java/com/tyman/KryptoMod/events/OnServerJoin.java
@@ -1,31 +1,23 @@
 package com.tyman.KryptoMod.events;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.ForgeVersion;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 
 public class OnServerJoin {
-    @SubscribeEvent
-    public void onEntityJoinWorld(EntityJoinWorldEvent event) {
-    	Logger log = LogManager.getLogger(Loader.instance().activeModContainer().getModId());
-    	log.info("Entify Joined world!");
-    	log.info(event.entity instanceof EntityPlayer);
-    	log.info(event.entity != null);
-        if (event.entity != null && event.entity instanceof EntityPlayer) {
-        	String updateStatus = ForgeVersion.getResult(Loader.instance().activeModContainer()).status.toString();
-        	if (updateStatus == "OUTDATED") {
-        		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.BLACK + "---------------------------");
-        		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.RED + "A new version of KryptoMod is availible! Go to KryptoMod in the mod options to update!");
-        		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.BLACK + "---------------------------");
-        	}
-        }
+	@SubscribeEvent
+    public void onLoad(WorldEvent.Load event)
+    {
+		String updateStatus = ForgeVersion.getResult(Loader.instance().activeModContainer()).status.toString();
+    	if (updateStatus == "OUTDATED") {
+    		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.BLACK + "---------------------------");
+    		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.RED + "A new version of KryptoMod is availible! Go to KryptoMod in the mod options to update!");
+    		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.BLACK + "---------------------------");
+    	}
     }
     
 }
