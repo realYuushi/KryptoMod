acbbb223472b7306e3549f2ae8ef968a8e7947eb
Tyler Beckan ohio <tbeckman530@gmail.com>
2020-07-22 14:06:17 -0400

    Add logging (idk if it actually works)

--- src/main/java/com/tyman/KryptoMod/events/OnServerJoin.java
+++ src/main/java/com/tyman/KryptoMod/events/OnServerJoin.java
@@ -7,12 +7,18 @@ import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 
 public class OnServerJoin {
     @SubscribeEvent
     public void onEntityJoinWorld(EntityJoinWorldEvent event) {
-        if (event.entity != null && event.entity instanceof EntityPlayer && !event.entity.worldObj.isRemote) {
+    	Logger log = LogManager.getLogger(Loader.instance().activeModContainer().getModId());
+    	log.info("Entify Joined world!");
+    	log.info(event.entity instanceof EntityPlayer);
+    	log.info(event.entity != null);
+        if (event.entity != null && event.entity instanceof EntityPlayer) {
         	String updateStatus = ForgeVersion.getResult(Loader.instance().activeModContainer()).status.toString();
         	if (updateStatus == "OUTDATED") {
         		Minecraft.getMinecraft().thePlayer.sendChatMessage(EnumChatFormatting.BLACK + "---------------------------");
